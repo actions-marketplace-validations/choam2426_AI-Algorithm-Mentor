978ac52f05977e6f2c2d79553f34ec18b48f9dc6
choam2426 <choam2426@gmail.com>
2025-07-23 22:47:01 +0900

    feature: main 함수 구현 및 코드 리뷰를 위한 문제 포맷팅 추가

--- app/main.py
+++ app/main.py
@@ -1,8 +1,25 @@
-from src import *
+from src.consts import REVIEW_LANGUAGE
+from src.github_service import get_current_commit_diff
+from src.online_judge_crawler import crawl_problems_from_text, format_problem_for_llm
+from src.prompt import prompt
+
+
+def main():
+    review_target_file_contents = get_current_commit_diff()
+    if not review_target_file_contents:
+        print("No files to review.")
+        return
+    for filepath, content in review_target_file_contents.items():
+        problem = crawl_problems_from_text(content)
+        if problem:
+            formatted_problem = format_problem_for_llm(problem[0])
+            print(f"Formatted Problem for LLM:\n{formatted_problem}")
+        prompt.format_prompt(
+            problem_description=formatted_problem,
+            user_code=content,
+            review_language=REVIEW_LANGUAGE,
+        )
+
 
 if __name__ == "__main__":
-    file_contents = get_commit_data(REPOSITORY_NAME, COMMIT_SHA, GITHUB_TOKEN)
-
-    llm_response = get_code_review_by_openai(api_key=OPENAI_API_KEY, file_contents=file_contents)
-
-    write_comment_in_commit(repository=REPOSITORY_NAME, commit_sha=COMMIT_SHA, token=GITHUB_TOKEN, comment=llm_response)
+    main()
