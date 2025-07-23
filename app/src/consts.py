a5ecdcad996f2ffa23b4e3d74eb59088bf312ff3
choam2426 <choam2426@gmail.com>
2025-07-23 21:08:17 +0900

    feature: 알고리즘 언어 목록 추가

--- app/src/consts.py
+++ app/src/consts.py
@@ -8,3 +8,4 @@ LLM_MODEL = os.getenv("LLM_MODEL")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
+ALGORITHM_LANGUAGES = [".c", ".cpp", "cc", "cxx", ".py", ".java", ".js", ".go", ".rs"]
