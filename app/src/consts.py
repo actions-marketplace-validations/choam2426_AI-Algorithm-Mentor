1fd96a49b776d7104dd6bf96263a85b1a8f2392e
choam2426 <choam2426@gmail.com>
2025-10-05 14:05:26 +0900

    변경: DEFAULT_MODEL_MAP의 값 형식을 리스트로 변경하고, OPENAI, ANTHROPIC, GOOGLE 모델에 대한 여러 버전을 추가했습니다. 또한, GOOGLE_API_KEY를 GEMINI_API_KEY로 변경했습니다.

--- app/src/consts.py
+++ app/src/consts.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import List
 
 SURPORT_FILE_EXTENSIONS: tuple = (
     ".c",
@@ -18,14 +19,15 @@ class LLMProvider(Enum):
     ANTHROPIC = "anthropic"
     GOOGLE = "google"
 
-DEFAULT_MODEL_MAP: dict[LLMProvider, str] = {
-    LLMProvider.OPENAI: "gpt-5",
-    LLMProvider.ANTHROPIC: "claude-4.0-sonnet",
-    LLMProvider.GOOGLE: "gemini-2.5-pro",
+
+DEFAULT_MODEL_MAP: dict[LLMProvider, List[str]] = {
+    LLMProvider.OPENAI: ["gpt-5", "gpt-5-nano"],
+    LLMProvider.ANTHROPIC: ["claude-sonnet-4-5", "claude-sonnet-4-5"],
+    LLMProvider.GOOGLE: ["gemini-2.5-pro", "gemini-2.5-flash"],
 }
 
 API_KEY_ENV_MAP: dict[LLMProvider, str] = {
     LLMProvider.OPENAI: "OPENAI_API_KEY",
     LLMProvider.ANTHROPIC: "ANTHROPIC_API_KEY",
-    LLMProvider.GOOGLE: "GOOGLE_API_KEY",
+    LLMProvider.GOOGLE: "GEMINI_API_KEY",
 }
